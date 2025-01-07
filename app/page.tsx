00553fd29cd02f6378acbfceefaea98fa7e91cc4
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-01-07 18:58:16 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,28 @@ import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
 
+import { headers } from "next/headers";
+
 // import { VerifiedUser } from "@/lib/auth";
 // import { redirect } from "next/navigation";
 
 const serverConfig = getServerSideConfig();
+export function getCurrentDomain() {
+  const headersList = headers();
+  // 获取完整的 Host 信息
+  // const host = headersList.get('host')
+  // 获取协议
+  // const protocol = headersList.get('x-forwarded-proto') || 'http'
+  return headersList.get("host");
+}
 
 export default async function App() {
   // const isUser = await VerifiedUser();
   // if (!isUser) {
   //   redirect("/login");
   // }
+  const host = await getCurrentDomain();
+  console.log("-----", host);
 
   return (
     <>
