a138813ccb25e96d7c515f8008d6c0bbbe09682a
sijinhui <sijinhui@qq.com>
2025-01-19 20:03:01 +0800

    更新公告

--- app/components/customNotice.tsx
+++ app/components/customNotice.tsx
@@ -12,28 +12,28 @@ export const CustomNotice = () => {
   // const [host, setHost] = useState("");
   const clientVersion = getClientConfig()?.version ?? "";
   const clientVersionKey = "announcementDismissed";
-  const RE_SHOW_DAYS = 1; // 间隔天数
+  const RE_SHOW_DAYS = 7; // 间隔天数
   const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
 
   // 直接强制重定向, 后续把功能做到nginx中
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // 当前时间
-      const now = new Date();
-      // 对比目标时间（2025年2月1日）
-      const target = new Date("2025-02-01");
-      const host = window.location.hostname;
-      // 如果当前时间大于目标时间，就直接重定向
-      if (
-        now > target &&
-        host &&
-        (host === "localhost" || host === "chat.xiaosi.cc")
-      ) {
-        router.push("https://si.icu");
-      }
-    }
-  }, [router]);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     // 当前时间
+  //     const now = new Date();
+  //     // 对比目标时间（2025年2月1日）
+  //     const target = new Date("2025-02-01");
+  //     const host = window.location.hostname;
+  //     // 如果当前时间大于目标时间，就直接重定向
+  //     if (
+  //       now > target &&
+  //       host &&
+  //       (host === "localhost" || host === "chat.xiaosi.cc")
+  //     ) {
+  //       router.push("https://si.icu");
+  //     }
+  //   }
+  // }, [router]);
 
   useEffect(() => {
     // 检查 localStorage 中存储的已关闭版本
@@ -95,9 +95,13 @@ export const CustomNotice = () => {
     >
       <Typography>
         <Paragraph>亲爱的用户们，👋</Paragraph>
-        为方便记忆，且刚好有个不错的域名闲置。
         <Paragraph>
-          我的网站即将更换域名，从 <Link strong>chat.xiaosi.cc</Link> 切换到{" "}
+          该开源项目作者不积极维护，很多gpt新功能无法使用，因此决定该网站也不再更新。
+          将使用新网址配合全新的项目提供<Text strong>更好的服务体验</Text>。
+        </Paragraph>
+
+        <Paragraph>
+          从 <Link strong>chat.xiaosi.cc</Link> 切换到{" "}
           <Link strong>si.icu</Link>。✨
         </Paragraph>
         <Paragraph>
@@ -105,12 +109,11 @@ export const CustomNotice = () => {
           <Text type="danger">2025年2月1日</Text> 前完成以下操作：
         </Paragraph>
         <ul>
-          <li>📁 设置-本地数据中导出您的重要聊天记录</li>
-          <li>🔐 确认您现在使用的账号，在左下角可以查看或者注销后尝试登录</li>
+          <li>📁 备份您的重要聊天记录</li>
           <li>
-            🔗 更新书签以便快速访问新域名
-            <Text type="secondary">（不更换后续也会正常跳转）</Text>
+            🔐 <Text strong>提供我您使用的邮箱</Text>以便于在新的站点中创建账号
           </li>
+          <li>🔗 更新书签以便快速访问新域名</li>
         </ul>
         <Paragraph>
           如有任何疑问或需要帮助，请随时联系我。感谢您的理解与支持！🌟
